ce1fe80aa6b795f4748c970f1ed9a8b2b4b36d75
ian <sylvester16@gmail.com>
2022-06-12 20:02:13 -0400

    quietly banging my head against a wall

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route exact path="/" element={<Home />}></Route>
+      <Route exact path="/about" element={<About />}></Route>
+    </Routes>
+  );
+}
+
+export default App;
