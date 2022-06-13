52a83222e55b5010a5451bb01f4b44d1a176767e
ian <sylvester16@gmail.com>
2022-06-13 16:31:35 -0400

    random

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
+
 
 const App = () => {
   return (
